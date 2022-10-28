59c61e144afb8b533bdcf9a8392d2440a731cf45
Saad MOUSTAKIM <saadmoustakim2@gmail.com>
2022-10-28 11:28:47 +0100

    EX

--- src/main/java/ma/enset/moustakim_saad_exam_blanc/MoustakimSaadExamBlancApplication.java
+++ src/main/java/ma/enset/moustakim_saad_exam_blanc/MoustakimSaadExamBlancApplication.java
@@ -1,5 +1,6 @@
 package ma.enset.moustakim_saad_exam_blanc;
 
+import ma.enset.moustakim_saad_exam_blanc.DessinPackage.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoustakimSaadExamBlancApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MoustakimSaadExamBlancApplication.class, args);
+
+
     }
 
 }
